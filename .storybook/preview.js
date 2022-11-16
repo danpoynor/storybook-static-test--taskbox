ce7600216b68e984761d648f0fde52c8c7d61dd4
Dan Poynor <danpoynor@gmail.com>
2022-11-16 16:19:15 -0600

    Add A11y addon and fixed an issue

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import '../src/index.css';
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+//
+// - parameters are typically used to control the behavior of Storybook's features and addons.
+// In our case, we're going to use them to configure how the actions (mocked callbacks) are handled.
+//
+// - The actions addon allows you to create callbacks that appear in the actions panel of the Storybook UI when clicked.
+// - actions allows us to create callbacks that appear in the actions panel of the Storybook UI when clicked.
+// So when we build a pin button, we'll be able to determine if a button click is successful in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
